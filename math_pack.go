9c59a855d3f0298c9078c3875de80e0272bff516
Pranay Joshi <35572191+pranayteaches@users.noreply.github.com>
2020-03-21 13:45:18 +0530

    Update math_pack.go

--- math_pack.go
+++ math_pack.go
@@ -7,8 +7,24 @@ import (
 
 // using math.Sqrt
 func main() {
-	var num float64 = 4 // using
+	// using float64 as math.Sqrt only accept float value
+	var num float64 = 4
 	fmt.Println("Pranay", num)
+
 	// using Printf to print in a format
-	fmt.Printf("The output is %.2f", math.Sqrt(num)) // using %.2f to print the first 2 decimal values of a float 64
+	var result = math.Sqrt(num)
+
+	fmt.Printf("The output is %.2f\n", result) // using %.2f to print the first 2 decimal values of a float 64
+
+	// rouding off to the nearest
+	var roundres = math.Round(result)
+	fmt.Println(roundres)
+
+	// rounding of to the top one
+	var ceilres = math.Ceil(result)
+	fmt.Println(ceilres)
+
+	// rounding of to the bottom one
+	var floorres = math.Floor(result)
+	fmt.Println(floorres)
 }
